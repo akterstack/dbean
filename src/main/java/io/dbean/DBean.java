6f643825c90c8503be42498cfbd611d41f649b95
shg@dsi <akter.hossain@dsinnovators.com>
2016-11-10 13:30:19 +0600

    testing

--- src/main/java/io/dbean/DBean.java
+++ src/main/java/io/dbean/DBean.java
@@ -11,11 +11,11 @@ public abstract class DBean<T> implements Serializable, Hackable {
     private Map<Namespace, Object> propertyValuesMap = new HashMap<>();
 
     public <V> V get(Namespace namespace) {
-        return (V)propertyValuesMap.get(namespace);
+        return applyFilter("get", this.getClass(), (V)propertyValuesMap.get(namespace));
     }
 
-    public T set(Namespace namespace, Object value) {
-        propertyValuesMap.put(namespace, value);
+    public <V> T set(Namespace namespace, V value) {
+        propertyValuesMap.put(namespace, applyFilter("set", this.getClass(), value));
         return (T)this;
     }
 
