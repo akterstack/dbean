c15918af7c91a79afa64543930adfc40747d7e40
jRoadie <akter.hossain.ict@gmail.com>
2016-11-30 00:58:34 +0600

    moving namespace code to namespaced branch

---
+++ src/test/groovy/io/dbean/test/mockdbeans/MockDBean.groovy
@@ -0,0 +1,15 @@
+package io.dbean.test.mockdbeans
+
+
+class MockDBean {
+
+    Long id
+
+    String name
+    String username
+    String password
+    String description
+
+    Date birthday
+
+}
--- src/test/groovy/io/dbean/test/mockdbeans/Person.groovy
+++
@@ -1,8 +0,0 @@
-package io.dbean.test.mockdbeans
-
-
-class Person {
-
-
-
-}
